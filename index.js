fb9f2603d31063660d5b26aeb03b92dd3ed70ff0
RAHUL14KUMAR <rahul.kumar.it25@heritageit.edu.in>
2026-03-01 18:20:11 +0530

    changes

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const app = express();
 app.get('/', (req, res) => {
   res.send("AWS Node App Running 🚀");
 });
+
+app.get('/db-test', async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 connectDB();
 app.listen(3000, () => {
   console.log("Server started on port 3000");
